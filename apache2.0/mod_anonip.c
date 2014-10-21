df9b4c7a9650e9e944c5fb3be6c151a40ac274ee
letorbi <torben.letorbi@gmail.com>
2014-10-21 11:23:31 +0200

    Rename removeip to anonip

--- apache2.0/mod_anonip.c
+++ apache2.0/mod_anonip.c
@@ -7,14 +7,14 @@
 #include "apr_strings.h"
 #include <arpa/inet.h>
 
-module AP_MODULE_DECLARE_DATA removeip_module;
+module AP_MODULE_DECLARE_DATA anonip_module;
 
 typedef struct {
     int enable;
-} removeip_server_cfg;
+} anonip_server_cfg;
 
-static void *removeip_create_server_cfg(apr_pool_t *p, server_rec *s) {
-    removeip_server_cfg *cfg = (removeip_server_cfg *)apr_pcalloc(p, sizeof(removeip_server_cfg));
+static void *anonip_create_server_cfg(apr_pool_t *p, server_rec *s) {
+    anonip_server_cfg *cfg = (anonip_server_cfg *)apr_pcalloc(p, sizeof(anonip_server_cfg));
     if (!cfg)
         return NULL;
 
@@ -23,10 +23,10 @@ static void *removeip_create_server_cfg(apr_pool_t *p, server_rec *s) {
     return (void *)cfg;
 }
 
-static const char *removeip_enable(cmd_parms *cmd, void *dummy, int flag) {
+static const char *anonip_enable(cmd_parms *cmd, void *dummy, int flag) {
     server_rec *s = cmd->server;
-    removeip_server_cfg *cfg = (removeip_server_cfg *)ap_get_module_config(s->module_config, 
-                                                                   &removeip_module);
+    anonip_server_cfg *cfg = (anonip_server_cfg *)ap_get_module_config(s->module_config, 
+                                                                   &anonip_module);
 
     cfg->enable = flag;
     return NULL;
@@ -36,8 +36,8 @@ static int change_remote_ip(request_rec *r) {
     const char *fwdvalue;
     char *val;
     struct in_addr ip;
-    removeip_server_cfg *cfg = (removeip_server_cfg *)ap_get_module_config(r->server->module_config,
-                                                                   &removeip_module);
+    anonip_server_cfg *cfg = (anonip_server_cfg *)ap_get_module_config(r->server->module_config,
+                                                                   &anonip_module);
     if (!cfg->enable)
         return DECLINED;
 
@@ -54,13 +54,13 @@ static int change_remote_ip(request_rec *r) {
     return DECLINED;
 }
 
-static const command_rec removeip_cmds[] = {
+static const command_rec anonip_cmds[] = {
     AP_INIT_FLAG(
-                 "REMOVEIPenable",
-                 removeip_enable,
+                 "AnonipEnable",
+                 anonip_enable,
                  NULL,
                  RSRC_CONF,
-                 "Enable mod_removeip"
+                 "Enable mod_anonip"
                  ),
     { NULL }
 };
@@ -69,12 +69,12 @@ static void register_hooks(apr_pool_t *p) {
     ap_hook_post_read_request(change_remote_ip, NULL, NULL, APR_HOOK_MIDDLE);
 }
 
-module AP_MODULE_DECLARE_DATA removeip_module = {
+module AP_MODULE_DECLARE_DATA anonip_module = {
     STANDARD20_MODULE_STUFF,
     NULL,
     NULL,
-    removeip_create_server_cfg,
+    anonip_create_server_cfg,
     NULL,
-    removeip_cmds,
+    anonip_cmds,
     register_hooks,
 };
